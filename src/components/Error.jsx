419a133d289f36005998b72ae653b03017fddae0
Jhon <jhon.naib327@gmail.com>
2022-05-01 13:44:12 -0500

    f

---
+++ src/components/Error.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const error = ({children}) => {
+  return (
+    <div className="bg-red-800 text-white text-center p-3 uppercase font-bold mb-3 rounded-md">
+      <p>{children}</p>
+    </div>
+  );
+};
+
+export default error;
